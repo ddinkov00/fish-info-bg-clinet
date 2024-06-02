846ba8e1e750cc55b72712386cab4a816d7393bb
Dimitar Dinkov <dimitar.dinkov@othis.com>
2024-06-02 16:03:13 +0300

    Auth redirect

--- src/utils/authenticationHelper.ts
+++ src/utils/authenticationHelper.ts
@@ -1,18 +1,25 @@
+import Router from 'next/router';
+
 import { login, refreshToken } from '@/api/identityController';
 import { LoginRequest } from '@/api/types/identity';
 
-import { LocalStorageKeys } from './constants';
+import { LocalStorageKeys, Routes } from './constants';
 
 export const handelUnauthorized = async () => {
   const refreshTokenValue = localStorage.getItem(LocalStorageKeys.refreshToken);
 
   if (!refreshTokenValue) {
+    Router.push(Routes.login);
     return;
   }
 
-  const refreshTokenData = await refreshToken({ refreshToken: refreshTokenValue });
-  localStorage.setItem(LocalStorageKeys.accessToken, refreshTokenData.accessToken);
-  localStorage.setItem(LocalStorageKeys.refreshToken, refreshTokenData.refreshToken);
+  try {
+    const refreshTokenData = await refreshToken({ refreshToken: refreshTokenValue });
+    localStorage.setItem(LocalStorageKeys.accessToken, refreshTokenData.accessToken);
+    localStorage.setItem(LocalStorageKeys.refreshToken, refreshTokenData.refreshToken);
+  } catch (error) {
+    Router.push(Routes.login);
+  }
 };
 
 export const handleLogin = async (email: string, password: string) => {
@@ -24,4 +31,6 @@ export const handleLogin = async (email: string, password: string) => {
   const loginData = await login(request);
   localStorage.setItem(LocalStorageKeys.accessToken, loginData.accessToken);
   localStorage.setItem(LocalStorageKeys.refreshToken, loginData.refreshToken);
+
+  Router.push(Routes.login);
 };
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,5 +2,11 @@ export const EMPTY_STRING = '';
 
 export const LocalStorageKeys = {
   accessToken: 'accessToken',
-  refreshToken: 'refreshToken'
+  refreshToken: 'refreshToken',
+};
+
+export const Routes = {
+  login: '/login',
+  register: '/register',
+  home: '/home',
 };
